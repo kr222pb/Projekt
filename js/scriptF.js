22d67325c5a562f33d25462363bc6ff59d3d7824
Frida Bergströmn <fb223es@student.lnu.se>
2024-04-30 21:19:10 +0200

    karta klar

--- js/scriptF.js
+++ js/scriptF.js
@@ -2,6 +2,7 @@
 let currentindex = 0;
 let selectedActivities = [];
 let obj;
+let map
 
 function init() {
     fetchSMAPI()
@@ -10,6 +11,8 @@ function init() {
 
     let kryss = document.querySelector("#kryss")
     kryss.addEventListener("click", nextSlide)
+
+    document.querySelector("#info").addEventListener("click",makeMap)
 
 }
 
@@ -41,29 +44,75 @@ async function fetchSMAPI() {
 
 }
 
+let lat
+let lng
+
 function readSMAPI(data) {
     let HTMLCode = ""
     // Iterera över datan och logga namnet på varje objekt
     for (let x = 0; x < data.payload.length; x++) {
         obj = data.payload[currentindex]
 
+        //Skriva koden
         HTMLCode = "<h2>" + obj.name + "</h2>"
         HTMLCode += "<h3>" + obj.description + "</h3>"
         HTMLCode += "<h4>" + obj.abstract + "</h4>"
         let h2 = document.querySelector(".container h2")
         h2.innerHTML = HTMLCode
+        lat = obj.lat
+        lng = obj.lng
+        console.log(lat,lng)
+       
     }
-
+ makeMap(lat,lng)
 }
-function nextSlide(e){
+
+
+function nextSlide(e) {
     console.log("Annans")
-    if(e.target.id == "bock"){
+    if (e.target.id == "bock") {
         console.log("Val gjort")
         let kiwi = obj.name
         selectedActivities.push(kiwi)
         console.log(selectedActivities)
+
     }
-  
+
     currentindex++
     fetchSMAPI()
+}
+
+
+// Skapa en karta
+
+function makeMap(lat, lng) {
+    // Skapa en ikon för markören
+    let icon = L.icon({
+        iconUrl: 'bilder/plats.svg',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -76]
+    });
+
+    // Skapa kartan om den inte redan är skapad
+    if (!map) {
+        map = L.map("map").setView([lat, lng], 10)
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map)
+
+        // Skapa markören och placera den på kartan
+       marker = L.marker([lat, lng], { icon: icon }).addTo(map)
+    } else {
+        // Flytta kartans vy till de nya koordinaterna
+        map.setView([lat, lng])
+
+        // Ta bort den befintliga markören från kartan
+        map.removeLayer(marker)
+
+        // Skapa en ny markör och placera den på den nya positionen med din ikon
+        marker = L.marker([lat, lng], { icon: icon }).addTo(map)
+    }
 }
