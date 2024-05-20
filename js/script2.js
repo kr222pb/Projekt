06ba74e05459b240161d9ce23a32bcc76f693298
Kseniya Rahouskaya <kr222pb@student.lnu.se>
2024-05-20 14:42:00 +0200

    modal

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', async function() {
     const filterArrow = document.querySelector('.filter-arrow');
     const dropdownMenu = document.querySelector('.dropdown-menu');
@@ -6,14 +7,17 @@ document.addEventListener('DOMContentLoaded', async function() {
     const attractionDropdown = document.getElementById('attraktion');
     const locationDropdown = document.getElementById('places');
     const allDropdown = document.getElementById('all');
+    const homeEveningDropdown = document.getElementById('homeEvening');
     const listUtf = document.getElementById('listUtf');
     const messageDisplay = document.getElementById('messageDisplay');
     let allItemsActivated = false;
     let combinedData = [];
+    let streamingData = {};
     let selectedActivities = new Set();
     let selectedFoods = new Set();
     let selectedAttractions = new Set();
     let selectedLocations = new Set();
+    let selectedStreamingServices = new Set();
 
     async function fetchAllEstablishmentData() {
         const url = `https://smapi.lnu.se/api/?api_key=61fTJHBb&controller=establishment&method=getall`;
@@ -28,7 +32,20 @@ document.addEventListener('DOMContentLoaded', async function() {
         }
     }
 
+    async function fetchStreamingData() {
+        const url = 'data/movie.json'; 
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error("Fel vid hämtning av data");
+            streamingData = await response.json();
+        } catch (error) {
+            console.error("Fel vid hämtning:", error);
+            messageDisplay.textContent = "Kunde inte ladda data.";
+        }
+    }
+
     await fetchAllEstablishmentData();
+    await fetchStreamingData();
 
     function toggleDropdown(dropdown, menu, selectedSet, type) {
         dropdown.addEventListener('click', function(event) {
@@ -42,7 +59,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 
     function closeOtherMenus(openedMenu) {
-        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown].forEach(dropdown => {
+        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown, homeEveningDropdown].forEach(dropdown => {
             const menu = dropdown.querySelector('.nested-dropdown-menu');
             if (menu !== openedMenu) {
                 menu.classList.remove('active');
@@ -54,9 +71,10 @@ document.addEventListener('DOMContentLoaded', async function() {
     toggleDropdown(foodDropdown, foodDropdown.querySelector('.nested-dropdown-menu'), selectedFoods, "food");
     toggleDropdown(attractionDropdown, attractionDropdown.querySelector('.nested-dropdown-menu'), selectedAttractions, "attraction");
     toggleDropdown(locationDropdown, locationDropdown.querySelector('.nested-dropdown-menu'), selectedLocations, "location");
+    toggleDropdown(homeEveningDropdown, homeEveningDropdown.querySelector('.nested-dropdown-menu'), selectedStreamingServices, "streaming");
 
     document.addEventListener('click', function(event) {
-        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown].forEach(dropdown => {
+        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown, homeEveningDropdown].forEach(dropdown => {
             const menu = dropdown.querySelector('.nested-dropdown-menu');
             if (!dropdown.contains(event.target)) {
                 menu.classList.remove('active');
@@ -75,25 +93,31 @@ document.addEventListener('DOMContentLoaded', async function() {
         const allowedTypes = ["activity", "food", "attraction"];
         const excludedDescriptions = ["Lekplats", "kyrka", "Lekland", "Hamburgerkedja", "Hälsocenter", "Golfbana"];
         const uniqueItems = new Set();
-    
-        combinedData.forEach(item => {
-            if (type === "location") {
-                const location = item.city || item.province;
-                if (location) {
-                    uniqueItems.add(location);
-                }
-            } else {
-                if (allowedTypes.includes(item.type) && !excludedDescriptions.some(desc => item.description?.toLowerCase().includes(desc.toLowerCase()))) {
-                    if (item.type === type) {
-                        uniqueItems.add(item.description);
+
+        if (type === "streaming") {
+            Object.keys(streamingData).forEach(service => {
+                uniqueItems.add(service);
+            });
+        } else {
+            combinedData.forEach(item => {
+                if (type === "location") {
+                    const location = item.city || item.province;
+                    if (location) {
+                        uniqueItems.add(location);
+                    }
+                } else {
+                    if (allowedTypes.includes(item.type) && !excludedDescriptions.some(desc => item.description?.toLowerCase().includes(desc.toLowerCase()))) {
+                        if (item.type === type) {
+                            uniqueItems.add(item.description);
+                        }
                     }
                 }
-            }
-        });
-    
+            });
+        }
+
         // Sortera de unika platserna eller beskrivningarna
         const sortedItems = Array.from(uniqueItems).sort();
-    
+
         sortedItems.forEach(item => {
             const checkboxContainer = document.createElement('div');
             const checkbox = document.createElement('input');
@@ -101,32 +125,37 @@ document.addEventListener('DOMContentLoaded', async function() {
             checkbox.id = item; // Använd beskrivning eller plats som ID
             checkbox.name = `${type}-options`;
             checkbox.value = item;
-    
+
             const label = document.createElement('label');
             label.htmlFor = item;
             label.textContent = item;
-    
+
             checkbox.addEventListener('click', event => {
                 event.stopPropagation();
             });
-    
+
             label.addEventListener('click', event => {
-                event.stopPropagation(); // Förhindra att eventet bubblar upp när etiketten klickas
+                event.stopPropagation(); 
             });
-    
+
             checkbox.addEventListener('change', () => {
                 if (checkbox.checked) {
                     selectedSet.add(item);
+                    if (type === "streaming") {
+                        displayStreamingMovies(item);
+                    }
                 } else {
                     selectedSet.delete(item);
                 }
-                updateListDisplay(); // Uppdatera listan med de valda elementen
+                if (type !== "streaming") {
+                    updateListDisplay(); // Uppdatera listan med de valda elementen
+                }
             });
-    
+
             checkboxContainer.appendChild(checkbox);
             checkboxContainer.appendChild(label);
             menu.appendChild(checkboxContainer);
-    
+
             if (selectedSet.has(item)) {
                 checkbox.checked = true;
             }
@@ -134,7 +163,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
 
     function activateAllItems() {
-        [selectedActivities, selectedFoods, selectedAttractions, selectedLocations].forEach(set => {
+        [selectedActivities, selectedFoods, selectedAttractions, selectedLocations, selectedStreamingServices].forEach(set => {
             set.clear();
             combinedData.forEach(item => {
                 if (['activity', 'food', 'attraction'].includes(item.type)) {
@@ -143,21 +172,25 @@ document.addEventListener('DOMContentLoaded', async function() {
                 if (item.city) selectedLocations.add(item.city);
                 if (item.province) selectedLocations.add(item.province);
             });
+            Object.keys(streamingData).forEach(service => {
+                selectedStreamingServices.add(service);
+            });
         });
         updateCheckboxes();
     }
 
     function deactivateAllItems() {
-        [selectedActivities, selectedFoods, selectedAttractions, selectedLocations].forEach(set => set.clear());
+        [selectedActivities, selectedFoods, selectedAttractions, selectedLocations, selectedStreamingServices].forEach(set => set.clear());
         updateCheckboxes();
     }
 
     function updateCheckboxes() {
-        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown].forEach(dropdown => {
+        [activitiesDropdown, foodDropdown, attractionDropdown, locationDropdown, homeEveningDropdown].forEach(dropdown => {
             const menu = dropdown.querySelector('.nested-dropdown-menu');
             const set = dropdown === activitiesDropdown ? selectedActivities :
                        dropdown === foodDropdown ? selectedFoods :
-                       dropdown === attractionDropdown ? selectedAttractions : selectedLocations;
+                       dropdown === attractionDropdown ? selectedAttractions :
+                       dropdown === locationDropdown ? selectedLocations : selectedStreamingServices;
             const checkboxes = menu.querySelectorAll('input[type="checkbox"]');
             checkboxes.forEach(checkbox => checkbox.checked = set.has(checkbox.value));
         });
@@ -171,19 +204,19 @@ document.addEventListener('DOMContentLoaded', async function() {
         console.log("Uppdaterar visningen av listan...");
         const allowedTypes = ["activity", "food", "attraction"];
         const excludedDescriptions = ["Lekplats", "kyrka", "Lekland", "Hamburgerkedja", "Hälsocenter", "Golfbana"];
-    
-        const hasCategorySelected = selectedActivities.size > 0 || selectedFoods.size > 0 || selectedAttractions.size > 0;
+
+        const hasCategorySelected = selectedActivities.size > 0 || selectedFoods.size > 0 || selectedAttractions.size > 0 || selectedStreamingServices.size > 0;
         const hasLocationSelected = selectedLocations.size > 0;
         console.log(`Kategorier valda: ${hasCategorySelected}, Platser valda: ${hasLocationSelected}`);
-    
+
         listUtf.innerHTML = ''; // Rensa listan
-    
+
         // Om inga kategorier eller platser är valda, visa meddelande
         if (!hasCategorySelected && !hasLocationSelected) {
             messageDisplay.textContent = "Inga resultat matchar dina val.";
             return;
         }
-    
+
         // Filtrera data baserat på valda kriterier
         const filteredData = combinedData.filter(item => {
             const isTypeAllowed = allowedTypes.includes(item.type);
@@ -192,12 +225,12 @@ document.addEventListener('DOMContentLoaded', async function() {
             const matchesFood = selectedFoods.has(item.description) && item.type === 'food';
             const matchesAttraction = selectedAttractions.has(item.description) && item.type === 'attraction';
             const matchesLocation = selectedLocations.has(item.city) || selectedLocations.has(item.province);
-    
+
             return isTypeAllowed && isDescriptionAllowed && 
                    ((hasCategorySelected ? (matchesActivity || matchesFood || matchesAttraction) : true) &&
                     (hasLocationSelected ? matchesLocation : true));
         });
-    
+
         // Hantera om inga data passar filtreringen
         if (filteredData.length === 0) {
             messageDisplay.textContent = "Inga resultat matchar dina val.";
@@ -211,6 +244,18 @@ document.addEventListener('DOMContentLoaded', async function() {
             });
         }
     }
+
+    function displayStreamingMovies(service) {
+        const movies = streamingData[service];
+        listUtf.innerHTML = ''; // Rensa listan
+        movies.forEach(movie => {
+            const listItem = document.createElement('div');
+            listItem.classList.add('list-item');
+            listItem.innerHTML = `<h3>${movie.Title}</h3><p>Kategori: ${movie.Category}</p><p>Längd: ${movie.Length} min</p><p>Betyg: ${movie.Stars}</p>`;
+            listUtf.appendChild(listItem);
+        });
+    }
+
     filterArrow.addEventListener('click', function(event) {
         event.preventDefault();
         dropdownMenu.classList.toggle('active');
--- js/scriptUtf.js
+++ js/scriptUtf.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 let combinedData = []; // För att lagra all hämtad data
 let map;
 let marker;
+let lat, lng;
 
 async function fetchAllEstablishmentData() {
     const url = `https://smapi.lnu.se/api/?api_key=61fTJHBb&controller=establishment&method=getall`;
@@ -24,7 +25,7 @@ async function fetchAllEstablishmentData() {
 
 function setupEventListeners() {
     const searchInput = document.querySelector("#searchInput");
-    const filterArrow = document.querySelector("#filterArrow"); 
+    const filterArrow = document.querySelector("#filterArrow");
 
     if (searchInput) {
         searchInput.addEventListener("keypress", function(event) {
@@ -45,14 +46,17 @@ function setupEventListeners() {
 
     if (filterArrow) {
         filterArrow.addEventListener("click", function() {
-            searchInput.value = ""; // Rensa sökfält
-            updateListWithFilteredData([]); // Rensa listan
+            searchInput.value = "";
+            updateListWithFilteredData([]);
         });
     }
 
-    document.getElementById("modalClose").addEventListener("click", function() {
-        document.getElementById("modal").style.display = "none";
-    });
+    const modalClose = document.getElementById("modalClose");
+    if (modalClose) {
+        modalClose.addEventListener("click", function() {
+            document.getElementById("modal").style.display = "none";
+        });
+    }
 }
 
 function performSearch(query) {
@@ -81,23 +85,52 @@ function updateListWithFilteredData(filteredData) {
         return;
     }
 
-    listUtf.innerHTML = ""; // Rensa tidigare resultat
-    document.getElementById("messageDisplay").textContent = ""; // Rensa tidigare meddelanden
+    listUtf.innerHTML = "";
+    document.getElementById("messageDisplay").textContent = "";
 
     filteredData.forEach(item => {
-        const listItem = document.createElement("div");
-        listItem.classList.add("list-item");
-        listItem.innerHTML = `
-            <h3>${item.name}</h3>
-            <p class="itemDescr">${item.description || "Ingen beskrivning tillgänglig."}</p>
-            <p class="itemLocPr">Plats: ${item.city || item.province}, Prisnivå: ${item.price_range || "ej angiven"}</p>
-        `;
-        listItem.addEventListener("click", () => {
-            openModal();
-            updateMap(item.lat, item.lng); 
-        });
-        listUtf.appendChild(listItem);
+        if (item) {
+            const listItem = document.createElement("div");
+            listItem.classList.add("list-item");
+            listItem.innerHTML = `
+                <h3>${item.name}</h3>
+                <p class="itemDescr">${item.description || "Ingen beskrivning tillgänglig."}</p>
+                <p class="itemLocPr">Plats: ${item.city || item.province}, Prisnivå: ${item.price_range|| "ej angiven"}</p>
+            `;
+            listItem.addEventListener("click", () => {
+                updateImageContainer(item);
+                openModal();
+                updateMap(item.lat, item.lng);
+                lat = item.lat;
+                lng = item.lng;
+                checkForLocationUpdate(); // Uppdaterar vädret när modalen öppnas
+            });
+            listUtf.appendChild(listItem);
+        }
     });
+}
+
+function updateImageContainer(item) {
+    const imageContainer = document.querySelector(".image-container");
+    const imgSrc = chooseImg(item.description);
+    const imgElement = imageContainer.querySelector("img");
+    if (imgElement) {
+        imgElement.src = imgSrc;
+        imgElement.alt = item.description;
+    } else {
+        const newImgElement = document.createElement("img");
+        newImgElement.src = imgSrc;
+        newImgElement.alt = item.description;
+        imageContainer.appendChild(newImgElement);
+    }
+
+    // Uppdatera informationen
+    document.getElementById("activity-type").textContent = `Typ av aktivitet: ${item.type || "Ej angiven"}`;
+    document.getElementById("activity-city").textContent = `Stad: ${item.city || item.province || "Ej angiven"}`;
+    document.getElementById("activity-price").textContent = `Prisnivå: ${item.price_range || "Ej angiven"}`;
+    document.getElementById("activity-abstract").textContent = `Beskrivning: ${item.abstract || "Ingen beskrivning tillgänglig."}`;
+    document.getElementById("activity-reviews").textContent = `Recensioner: ${item.num_reviews || "Inga recensioner tillgängliga."}`;
+    document.getElementById("activity-rating").textContent = `Recensioner: ${item.rating || "Inga recensioner tillgängliga."}`;
 }
 
 function openModal() {
@@ -115,7 +148,7 @@ function updateMap(lat, lng) {
 
     const mapContainer = document.getElementById('map');
     if (mapContainer) {
-        mapContainer.innerHTML = ""; // Clear previous map content
+        mapContainer.innerHTML = "";
     }
 
     const icon = L.icon({
@@ -138,4 +171,107 @@ function updateMap(lat, lng) {
     }
 
     marker = L.marker([lat, lng], { icon: icon }).addTo(map);
+}
+
+function chooseImg(description) {
+    let a = ["Klippklättring"];
+    let b = ["Simhall"];
+    let c = ["Sevärdhet", "Fornlämning"];
+    let d = ["Älgpark", "Djurpark", "Temapark"];
+    let e = ["Glasbruk"];
+    let f = ["Konstgalleri", "Ateljé", "Konsthall"];
+    let g = ["Restaurang", "Bistro", "Pizzeria"];
+    let h = ["Gatukök"];
+    let i = ["Gokart"];
+    let j = ["Zipline"];
+    let k = ["Biograf"];
+    let l = ["Cafe"];
+    let m = ["Naturreservat"];
+    let n = ["Paintballcenter"];
+    let o = ["Nattklubb", "Bowlinghall", "Nöjescenter"];
+    let p = ["Hälsocenter"];
+    let q = ["Hembygdspark"];
+    let r = ["Museum", "Slott"];
+
+    let category;
+    if (a.includes(description)) {
+        category = "A";
+    } else if (b.includes(description)) {
+        category = "B";
+    } else if (c.includes(description)) {
+        category = "C";
+    } else if (d.includes(description)) {
+        category = "D";
+    } else if (e.includes(description)) {
+        category = "E";
+    } else if (f.includes(description)) {
+        category = "F";
+    } else if (g.includes(description)) {
+        category = "G";
+    } else if (h.includes(description)) {
+        category = "H";
+    } else if (i.includes(description)) {
+        category = "I";
+    } else if (j.includes(description)) {
+        category = "J";
+    } else if (k.includes(description)) {
+        category = "K";
+    } else if (l.includes(description)) {
+        category = "L";
+    } else if (m.includes(description)) {
+        category = "M";
+    } else if (n.includes(description)) {
+        category = "N";
+    } else if (o.includes(description)) {
+        category = "O";
+    } else if (p.includes(description)) {
+        category = "P";
+    } else if (q.includes(description)) {
+        category = "Q";
+    } else if (r.includes(description)) {
+        category = "R";
+    } else {
+        category = "Okänd kategori";
+    }
+
+    switch (category) {
+        case "A":
+            return "Foto/klippa.jpg";
+        case "B":
+            return "Foto/simhall.jpg";
+        case "C":
+            return "Foto/runsten.jpg";
+        case "D":
+            return "Foto/Djur.jpg";
+        case "E":
+            return "Foto/glaskonst.jpg";
+        case "F":
+            return "Foto/tavla.jpg";
+        case "G":
+            return "Foto/mat.jpg";
+        case "H":
+            return "Foto/gatukök.jpg";
+        case "I":
+            return "Foto/mållinje.jpg";
+        case "J":
+            return "Foto/träd.jpg";
+        case "K":
+            return "Foto/popcorn.jpg";
+        case "L":
+            return "Foto/bulle.jpg";
+        case "M":
+            return "Foto/stig.jpg";
+        case "N":
+            return "Foto/paintball.jpg";
+        case "O":
+            return "Foto/disco.jpg";
+        case "P":
+            return "Foto/hälsocenter.jpg";
+        case "Q":
+            return "Foto/hus.jpg";
+        case "R":
+            return "Foto/slott.jpg";
+        default:
+            return "bilder/logo.svg";
+    }
 }
