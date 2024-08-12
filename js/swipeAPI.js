9664113fd617be070e78e83201b8b1f02191e0bd
Theo Myrvold <theomyrvold4@hotmail.com>
2024-08-12 09:34:03 +0200

    map

--- js/swipeAPI.js
+++ js/swipeAPI.js
@@ -228,7 +228,7 @@ function makeMap(lat, lng) {
     });
 
     if (!map) {
-        map = L.map(mapContainer).setView([lat, lng], 10);
+        map = L.map(mapContainer).setView([lat, lng], 14);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
